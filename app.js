a73b404df76c29294ec20e6836228ce3aa33acd8
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-02-05 23:51:05 +0100

    Add person generator to app.js file

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const os = require('os');
 const fs = require('fs');
 
+const firstNames = {
+  male: ['Adam', 'Włodek', 'Paweł', 'Robert'],
+  female: ['Monika', 'Anastazja', 'Weronika', 'Edyta']
+};
+
 const genders = ['male', 'female'];
-const maleNames = [];
-const femaleNames = [];
-const lastNames = [];
+const lastNames = ['Włodarczyk', 'Kociołek', 'Potter', 'Grenger', 'Rivia', 'Morawiecki'];
+const minAge = 18;
+const maxAge = 78;
 
-const randChoice = (arr) => {
-  return (
-    Math.random(arr).toString()
-  );
+const generatePerson = function(){
+  const randomGender = genders[Math.floor(Math.random() * genders.length)];
+  const randomFirstName = firstNames[randomGender][Math.floor(Math.random() * firstNames[randomGender].length)];
+  const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+  const randomAge = Math.floor(Math.random() * (maxAge - minAge + 1)) + minAge;
+
+  return {
+    firstName: randomFirstName,
+    lastName: randomLastName,
+    age: randomAge,
+    gender: randomGender
+  };
 };
 
-const people = [];
+const generatePeople = function(value) {
+  const people = [];
 
-for(i=0; i<20; i++){
-  const onePeople = {
-    gender: randChoice(genders),
-    firstName: randChoice(maleNames),
-    lastName: Math.random(),
-    age: Math.random(),
+  for (let i = 0; i < value; i++){
+    people.push(generatePerson());
   }
-  people.push(onePeople);
-  console.log(onePeople);
+  return people;
 };
+
+const people = generatePeople(20);
+const data = JSON.stringify(people, null, 2);
+
+fs.writeFile('people.json', data, (err) => {
+  if (err) throw err;
+  console.log('File has been saved!');
+});
 
 console.log('Platform: ', os.platform());
 console.log('Arch: ', os.arch());
